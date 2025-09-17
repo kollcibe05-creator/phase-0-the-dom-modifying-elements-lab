98d510f6e726e0441561e437b5e0d39e56735430
Collins Kibet <kollcibe05@gmail.com>
2025-09-17 15:06:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent= "Collins Kibet is the champion"
+document.body.append(newHeader)
+
+
+
